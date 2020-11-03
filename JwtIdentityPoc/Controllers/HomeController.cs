6cebbeeaa73cd8464ccc7e06072b2d6800c1d519
David Gomes dos Santos <davidgomesdosantos@hotmail.com>
2020-11-03 10:40:28 -0300

    Primeiro Commit

---
+++ JwtIdentityPoc/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtIdentityPoc.Models;
+using JwtIdentityPoc.Repositories;
+using JwtIdentityPoc.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtIdentityPoc.Controllers
+{
+    [ApiController]
+    [Route("v1/account")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("teste")]
+        public string teste()
+        {
+            return "funcionou";
+        }
+
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody]User model)
+        {
+            // Recupera o usuário
+            var user = UserRepository.Get(model.Username, model.Password);
+
+            // Verifica se o usuário existe
+            if (user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            // Gera o Token
+            var token = TokenService.GenerateToken(user);
+
+            // Oculta a senha
+            user.Password = "";
+
+            // Retorna os dados
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+
+        [HttpGet]
+        [Route("anonymous")]
+        [AllowAnonymous]
+        public string Anonymous() => "Anônimo";
+
+        [HttpGet]
+        [Route("authenticated")]
+        [Authorize]
+        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
+
+        [HttpGet]
+        [Route("employee")]
+        [Authorize(Roles = "employee,manager")]
+        public string Employee() => "Funcionário";
+
+        [HttpGet]
+        [Route("manager")]
+        [Authorize(Roles = "manager")]
+        public string Manager() => "Gerente";
+    }
+}
